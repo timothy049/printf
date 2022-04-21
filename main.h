ab851433c4f471d339591d90bb5c89c7a229c4f0
timothy049 <commandsec1996@gmail.com>
2022-04-21 02:45:24 -0700

    printf

--- main.h
+++ main.h
@@ -1,90 +1,76 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
+
 #include <stdlib.h>
-#include <unistd.h>
-/**
- * struct buffer - buffer structure for our implimentation of printf
- * @buf: buffer to write characters
- * @tmpbuf: tmp buffer to write to before putting in buffer
- * @format: the string passed to our printf
- * @ap: the variadic address point
- * @bp: the current point in the buffer
- * @tp: the current point in the tmp buffer
- * @fp: the current point in the format
- * @printed: the number of chars printed from _write
- */
-typedef struct buffer
-{
-	char *buf;
-	char *tmpbuf;
-	const char *format;
-	va_list ap;
-	int bp;
-	int tp;
-	int fp;
-	unsigned int printed;
-} buffer;
-/**
- *struct handlers - handlers struct
- *@plus:'+' flag
- *@space:' ' flag
- *@hash:'#' flag
- *@zero:'0' flag
- *@minus:'-' handler
- *@width:width handler
- *@precision:precision handler
- *@h_mod:h modifier handler
- *@l_mod:l modifier handler
- */
-typedef struct handlers
-{
-	unsigned int plus;
-	unsigned int space;
-	unsigned int hash;
-	unsigned int zero;
-	unsigned int minus;
-	unsigned int width;
-	unsigned int precision;
-	unsigned int h_mod;
-	unsigned int l_mod;
-} han_s;
-/**
- * struct ops - a structure containing a char to compare with formatb modifiers
- * and then choose the right function when it matches
- * @operation: the char we want to compare to
- * @func: the address of the function we want to return if a char matches
- */
-typedef struct ops
-{
-	char operation;
-	int (*func)(va_list);
-} ops_f;
+#include <stdarg.h>
 
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+/* print_int */
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+
+/* print_bases */
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
+
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* printchar */
 int _printf(const char *format, ...);
+
+/* get_print */
+int (*get_print(char s))(va_list, flags_t *);
+
+/* get_flag */
+int get_flag(char s, flags_t *f);
+
+/* print_alpha */
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+
+/* write_funcs */
 int _putchar(char c);
-int (*get_func(char ch))(va_list);
-int print_char(va_list list);
-int print_string(va_list list);
-int print_nan(char ch1, char ch2);
-int print_num(va_list list);
-unsigned int base_len(unsigned int num, int base);
-int print_binary(va_list list);
-int print_octal(va_list list);
-int print_hex(va_list list);
-int print_Hex(va_list list);
-int print_S(va_list list);
-int *rev_string(char *s);
-char *hex_conv(int num);
-int print_p(va_list list);
-int print_unsigned(va_list list);
-int print_rot13(va_list list);
-void _write(buffer *b_r, char c);
-void _write_str(buffer *b_r, char *s);
-void _write_tmpbuf(buffer *b_r);
-int print_r(va_list list);
+int _puts(char *str);
 
+/* print_custom */
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
 
+/* print_address */
+int print_address(va_list l, flags_t *f);
 
+/* print_percent */
+int print_percent(va_list l, flags_t *f);
 
-#endif /*MAIN_H*/
+#endif
